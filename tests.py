a743a8ed6ea96a1732968328837845561e8e9a9f
Daria Trunova <trunova.bts@gmail.com>
2019-11-19 21:22:20 +0200

    mock fixed

--- tests.py
+++ tests.py
@@ -49,30 +49,21 @@ class TestSoldiers(unittest.TestCase):
 
     @patch("units.Soldier.attack_chance")
     def test_if_attack_chance_less_than_05(self, mock):
-        mock = Mock()
         mock.return_value = 0.1
         s = Soldier()
         s2 = Soldier()
-        s.attack_chance = mock
-        exp = s.xp
         s.attack(s2)
         self.assertEqual(s2.hp, 100)
-        self.assertEqual(s.xp, exp)
 
-    @patch("units.Soldier.attack_chance")
-    @patch("units.Soldier.damage")
-    def test_if_attack_chance_more_than_05(self, mock_attack_ch, mock_damage):
+    def test_if_attack_chance_more_than_05(self):
         s1 = Soldier()
         s2 = Soldier()
         exp = s1.xp
-        mock_attack_ch, mock_damage = Mock(), Mock()
-        mock_attack_ch.return_value = 0.9
-        mock_damage.return_value = 8.5
-        s1.attack_chance = mock_attack_ch
-        s1.damage = mock_damage
+        s1.attack_chance = Mock(return_value=0.6)
+        s1.damage = Mock(return_value=8.5)
         s1.attack(s2)
         self.assertEqual(s2.hp, 91.5)
-        self.assertNotEqual(s1.xp, exp)
+        self.assertEqual(s1.xp, exp+1)
         self.assertFalse(s1.is_active)
 
 
